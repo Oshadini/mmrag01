c8e29ac516793a57e669a61ca86aedcf47fa6892
Oshadini Kariyawasam <43576871+Oshadini@users.noreply.github.com>
2024-05-06 14:26:17 +0530

    Update MMRAG.py

--- MMRAG.py
+++ MMRAG.py
@@ -102,20 +102,20 @@ if uploaded_file is not None:
         image_output_dir_path=image_path
     )
 
-# Categorize elements by type
-def categorize_elements(raw_pdf_elements):
-    """
-    Categorize extracted elements from a PDF into tables and texts.
-    raw_pdf_elements: List of unstructured.documents.elements
-    """
-    tables = []
-    texts = []
-    for element in raw_pdf_elements:
-        if "unstructured.documents.elements.Table" in str(type(element)):
-            tables.append(str(element))
-        elif "unstructured.documents.elements.CompositeElement" in str(type(element)):
-            texts.append(str(element))
-    return texts, tables
+    # Categorize elements by type
+    def categorize_elements(raw_pdf_elements):
+        """
+        Categorize extracted elements from a PDF into tables and texts.
+        raw_pdf_elements: List of unstructured.documents.elements
+        """
+        tables = []
+        texts = []
+        for element in raw_pdf_elements:
+            if "unstructured.documents.elements.Table" in str(type(element)):
+                tables.append(str(element))
+            elif "unstructured.documents.elements.CompositeElement" in str(type(element)):
+                texts.append(str(element))
+        return texts, tables
 
 texts, tables = categorize_elements(pdf_elements)
 
